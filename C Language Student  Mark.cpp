e4e8e895d4cfd38717ef70cb3235fde106a24c63
rakeshv1204 <vaghela.rakesh.2007@gmail.com>
2024-07-31 21:54:46 +0530

    Add files via upload

---
+++ C Language Student  Mark.cpp
@@ -0,0 +1,13 @@
+This C program determines if a student has passed or failed an exam based on their marks compared to a predefined passing threshold.
+
+
+Given the current values (studentMarks = 75 and failThreshold = 40), the program will output: "This student has passed the exam."
+
+
+You can modify the studentMarks and failThreshold variables to test different scenarios and see the corresponding output.
+
+
+This code is a simple example of how to use conditional statements to make decisions based on the values of variables.
+
+Mini Tasting Project By Vaghela Rakesh 
+Thank you ??
